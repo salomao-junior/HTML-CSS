8f699ff3ef981a083c2f9021fe6d309da9f9144a
salomao-junior <salomao.junior@monks.com>
2025-04-02 14:56:18 -0300

    refactor(database): Adjusted script for request to JSON Server

--- js/database.js
+++ js/database.js
@@ -1,35 +1,40 @@
-document
-  .getElementById("subscribe_container_form")
-  .addEventListener("submit", (event) => {
-    event.preventDefault();
+const form = document.getElementById("subscribe_container_form");
 
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
-    const github = document.getElementById("github").value;
-    const phoneNumber = document.getElementById("phoneNumber").value;
+form.addEventListener("submit", formSubmit);
 
-    const newUser = {
-      firstName,
-      lastName,
-      email,
-      github,
-      phoneNumber,
-    };
+async function formSubmit(event) {
+  event.preventDefault();
 
-    fetch("http://localhost:3000/customers", {
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const github = document.getElementById("github").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+
+  const newUser = {
+    firstName,
+    lastName,
+    email,
+    github,
+    phoneNumber,
+  };
+
+  try {
+    const response = await fetch("http://localhost:3000/customers", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newUser),
-    })
-      .then((response) => response.json())
-      .then(() => {
-        alert("User saved successfully!");
-        document.getElementById("subscribe_container_form").reset();
-      })
-      .catch(() => {
-        alert("Error saving data.");
-      });
-  });
+    });
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    alert("User saved successfully!");
+    document.getElementById("subscribe_container_form").reset();
+  } catch (error) {
+    alert("Error saving data. Please try again.");
+  }
+}
